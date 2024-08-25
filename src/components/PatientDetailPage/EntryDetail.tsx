7b169648d100fe3e940c60b6d67f35df4b0d03ac
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-08-25 18:19:04 -0400

    Implemented fetching and populating diagnosis details

--- src/components/PatientDetailPage/EntryDetail.tsx
+++ src/components/PatientDetailPage/EntryDetail.tsx
@@ -1,17 +1,32 @@
-import { Entry } from '../../types';
+import { Diagnosis, Entry } from '../../types';
+import diagnosisService from '../../services/diagnoses';
+import { useState, useEffect } from 'react';
 
 interface Props {
   entry: Entry;
 }
 
 const EntryDetail = ({ entry }: Props) => {
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
+
+  useEffect(() => {
+    const fetchDiagnoses = async () => {
+      const diagnoses = await diagnosisService.getAll();
+      setDiagnoses(diagnoses);
+    };
+    void fetchDiagnoses();
+  }, []);
+
   return (
     <div>
       {entry.date} <em>{entry.description}</em>
       {entry.diagnosisCodes ? (
         <ul>
-          {entry.diagnosisCodes.map((d) => (
-            <li key={d}>{d}</li>
+          {entry.diagnosisCodes.map((diagnosisCode) => (
+            <li key={diagnosisCode}>
+              {diagnosisCode}{' '}
+              {diagnoses.find((d) => d.code === diagnosisCode)?.name}
+            </li>
           ))}
         </ul>
       ) : null}
